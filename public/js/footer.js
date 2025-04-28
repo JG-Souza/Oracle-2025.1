f632ee6b96e9788a1fc1810e406df09d49fd38fa
jvrangel11 <jvictorangel45@gmail.com>
2025-04-28 11:16:28 -0300

    Missao visao e valores Java Script primeira parte

---
+++ public/js/footer.js
@@ -0,0 +1,29 @@
+function mostrarMissao(idTexto){
+    const texto = document.getElementById(idTexto);
+    if (texto.style.display === "none") {
+        texto.style.display = "block";
+    }
+    else {
+        texto.style.display = "none";
+    }
+}
+
+function mostrarVisao(idTexto){
+    const texto = document.getElementById(idTexto);
+    if (texto.style.display === "none") {
+        texto.style.display = "block";
+    }
+    else {
+        texto.style.display = "none";
+    }
+}
+
+function mostrarValores(idTexto){
+    const texto = document.getElementById(idTexto);
+    if (texto.style.display === "none") {
+        texto.style.display = "block";
+    }
+    else {
+        texto.style.display = "none";
+    }
+}
