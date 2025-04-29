6677800b932f7aa0809ad4f0a49d076db3e79c1d
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2025-04-29 00:33:00 -0300

    exclui parte da Navbar da branch

--- public/js/Navbar.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-
-const links = document.querySelector(".navbar-links");
-
-menu.addEventListener("click", () => {
-    links.classList.toggle("active");
-})
