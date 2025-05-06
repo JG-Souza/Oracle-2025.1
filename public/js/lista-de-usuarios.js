4fc72b8d68c4eb44ec44bf58dc4dc6644e4d8349
jvrangel11 <jvictorangel45@gmail.com>
2025-05-06 08:23:13 -0300

    Estilização Finalizada da lista de usuarios, falta a responsividade.

--- public/js/lista-de-usuarios.js
+++ public/js/lista-de-usuarios.js
@@ -3,7 +3,12 @@ function abrirModal(idModal) {
     modal.style.display = "flex";
 }
 
-function fecharModal(idModal) {
+function fecharModal(idModal, idInputNome, idInputEmail) {
     const modal = document.getElementById(idModal);
     modal.style.display = "none";
+    const inputNome = document.getElementById(idInputNome);
+    const inputEmail = document.getElementById(idInputEmail)
+    inputNome.value = "";
+    inputEmail.value = "";
 }
+
