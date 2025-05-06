34f9b6f9e3a1b9f72b8bbe4807a7f90a6b2dd464
Toledo29 <gabrieltgb@gmail.com>
2025-05-06 00:32:44 -0300

    mudanças navbar

--- public/js/Navbar.js
+++ public/js/Navbar.js
@@ -1,7 +1,11 @@
-const menu = document.querySelector(".menu");
-
+const botao = document.querySelector(".navbar-botao");
+const header = document.querySelector("header")
 const links = document.querySelector(".navbar-links");
+const body = document.querySelector("body");
 
-menu.addEventListener("click", () => {
+botao.addEventListener("click", () => {
+    botao.classList.toggle("active");
     links.classList.toggle("active");
+    header.classList.toggle("active");
+    body.classList.toggle("active");
 })
