20220b117af06fbd18bb56716a5d05127c21db40
jvrangel11 <jvictorangel45@gmail.com>
2025-05-06 10:12:48 -0300

    retirada a logo code assinatura

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,68 +1,68 @@
 function mostrarMissao(idTexto){
-    const texto = document.getElementById(idTexto);
-    if (texto.style.display === "none") {
-        texto.style.display = "block";
-    }
-    else {
-        texto.style.display = "none";
-    }
+  const texto = document.getElementById(idTexto);
+  if (texto.style.display === "none") {
+      texto.style.display = "block";
+  }
+  else {
+      texto.style.display = "none";
+  }
 }
 
 function mostrarVisao(idTexto){
-    const texto = document.getElementById(idTexto);
-    if (texto.style.display === "none") {
-        texto.style.display = "block";
-    }
-    else {
-        texto.style.display = "none";
-    }
+  const texto = document.getElementById(idTexto);
+  if (texto.style.display === "none") {
+      texto.style.display = "block";
+  }
+  else {
+      texto.style.display = "none";
+  }
 }
 
 function mostrarValores(idTexto){
-    const texto = document.getElementById(idTexto);
-    if (texto.style.display === "none") {
-        texto.style.display = "block";
-    }
-    else {
-        texto.style.display = "none";
-    }
+  const texto = document.getElementById(idTexto);
+  if (texto.style.display === "none") {
+      texto.style.display = "block";
+  }
+  else {
+      texto.style.display = "none";
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.missao-visao-e-valores a');
-    const logoImg = document.querySelector('.logo img');
-    const spans = document.querySelectorAll('.logo span');
+  const links = document.querySelectorAll('.missao-visao-e-valores a');
+  const logoImg = document.querySelector('.logo img');
+  const spans = document.querySelectorAll('.logo span');
+
+  links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
   
-    links.forEach(link => {
-      link.addEventListener('mouseenter', () => {
-    
-        logoImg.style.opacity = '0';
-        logoImg.style.visibility = 'hidden';
-  
-        spans.forEach(span => {
-          span.style.opacity = '0';
-          span.style.visibility = 'hidden';
-        });
-  
-        
-        const id = link.id.replace('-link', '');
-        const mostraTexto = document.getElementById(id);
-        if (mostraTexto) {
-            mostraTexto.style.opacity = '1';
-            mostraTexto.style.visibility = 'visible';
-        }
+      logoImg.style.opacity = '0';
+      logoImg.style.visibility = 'hidden';
+
+      spans.forEach(span => {
+        span.style.opacity = '0';
+        span.style.visibility = 'hidden';
       });
-  
-      link.addEventListener('mouseleave', () => {
-        
-        logoImg.style.opacity = '1';
-        logoImg.style.visibility = 'visible';
-  
-        
-        spans.forEach(span => {
-          span.style.opacity = '0';
-          span.style.visibility = 'hidden';
-        });
+
+      
+      const id = link.id.replace('-link', '');
+      const mostraTexto = document.getElementById(id);
+      if (mostraTexto) {
+          mostraTexto.style.opacity = '1';
+          mostraTexto.style.visibility = 'visible';
+      }
+    });
+
+    link.addEventListener('mouseleave', () => {
+      
+      logoImg.style.opacity = '1';
+      logoImg.style.visibility = 'visible';
+
+      
+      spans.forEach(span => {
+        span.style.opacity = '0';
+        span.style.visibility = 'hidden';
       });
     });
   });
+});
