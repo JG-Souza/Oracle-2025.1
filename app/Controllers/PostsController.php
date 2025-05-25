60e6371b358fe272ad61d350c65cc95b73bd5e34
LuisOtavio <luisotaviomaiabarrientos@gmail.com>
2025-05-24 21:01:21 -0300

    Implementação na tabela de posts e posts controller, ainda faltam alguns detalhes para funcionar perfeitamente

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -30,4 +30,39 @@ class PostsController
 
         header('Location: /tabela-de-posts');
     }
+
+    
+    public function edit($id)
+    {
+        $post = App::get('database')->selectById('posts', $id);
+        return view('admin/editar-post', compact('post'));
+    }
+
+    public function update($id)
+    {
+        $parameters = [
+            'title'         => $_POST['titulo'],
+            'origin'        => $_POST['origem'],
+            'story'         => $_POST['historia'],
+            'curiosity'     => $_POST['curiosidades'],
+            'lesson'        => $_POST['licoes'],
+            'reference'     => $_POST['referencias'],
+            'is_in_carousel'=> isset($_POST['is_in_carousel']) ? 1 : 0,
+            'img_path'      => $_POST['img_path'],
+            'user_id'       => $_POST['user_id'], // ou $_SESSION['user_id']
+        ];
+
+        App::get('database')->update('posts', $id, $parameters);
+
+        header('Location: /tabela-de-posts');
+        exit;
+    }
+
+    public function destroy($id)
+    {
+        App::get('database')->delete('posts', $id);
+
+        header('Location: /tabela-de-posts');
+        exit;
+    }
 }
