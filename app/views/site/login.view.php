1029a32c3d140ddbcdfa5d6ab022aded45bfd7ae
Toledo29 <gabrieltoledo.barreto@estudante.ufjf.br>
2025-06-09 00:35:57 -0300

    conserta pagina de login quebrada

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -26,41 +26,39 @@
             <div class="login-card-logo">
                 <img width="100%" height="100%" src="/public/assets/medusa.png" alt="Logo">
             </div>
-            <div class="login-card-conteudo">
-                <div>
-                    <form action="/login" method="POST">
-                        <div>
-                            <div id="" class="campo-text-input">
-                                <span>E-mail</span>
-                                <div class="email-input">
-                                    <input placeholder="seuemail@gmail.com" class="input-texto" type="text" name="email" id="email" autocomplete="off">
-                                </div>
+            <div>
+                <form class="login-form" action="/login" method="POST">
+                    <div class="login-card-conteudo">
+                        <div id="" class="campo-text-input">
+                            <span>E-mail</span>
+                            <div class="email-input">
+                                <input placeholder="seuemail@gmail.com" class="input-texto" type="text" name="email" id="email" autocomplete="off">
                             </div>
-                            <div id="" class="campo-text-input">
-                                <span>Senha</span>
-                                <div class="senha-input">
-                                    <input placeholder="***********" class="input-texto" type="password" name="senha" id="senha" autocomplete="off">
-                                    <i class="bi bi-eye" id="btn-senha" ></i>
-                                </div>
+                        </div>
+                        <div id="" class="campo-text-input">
+                            <span>Senha</span>
+                            <div class="senha-input">
+                                <input placeholder="***********" class="input-texto" type="password" name="senha" id="senha" autocomplete="off">
+                                <i class="bi bi-eye" id="btn-senha" ></i>
                             </div>
                             <div class="mensagem-erro">
                                 <p>
-                                    <?php
-                                    session_start();
-                                    if(isset($_SESSION['mensagem-erro']))
-                                    echo $_SESSION['mensagem-erro'];
-                                unset($_SESSION['mensagem-erro']);
-                                ?>
+                                <?php
+                                        session_start();
+                                        if(isset($_SESSION['mensagem-erro']))
+                                        echo $_SESSION['mensagem-erro'];
+                                    unset($_SESSION['mensagem-erro']);
+                                    ?>
                                 </p>
                             </div>
-                            <div class="campo-botao">
-                            <button class="botao" type="submit">Login</button>
-                            </div>
                         </div>
-                    </form>
-                </div>
+                        <div class="campo-botao">
+                            <button class="botao" type="submit">Login</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            </div>
+        </div>
     </main>
     
 </body>
